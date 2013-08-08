c1813afe535cdaa01965c11b66c5a7b98cc77ddf
falico <falico@gmail.com>
2013-08-08 00:11:44 -0300

    working! missing real entries and json database

--- cellmapper/src/main/webapp/js/pibCellMapper.js
+++ cellmapper/src/main/webapp/js/pibCellMapper.js
@@ -38,39 +38,36 @@ PIB.createMap = function(element, center) {
 			return marker;
 		}
 
+		var getMeeting = function(meeting) {
+			var formatted = '<ul class="meetingDays">';
+
+			if (meeting !== undefined) {
+				for (var i = 0; i < meeting.length; i++) {
+					formatted += '<li>'+meeting[i].day + ': ' + meeting[i].time+'</li>';
+				}
+			}
+
+			formatted += '</ul>';
+
+			return formatted;
+		}
+
 		var addNote = function(marker, note) {
 			var buildNote = function() {
+				var color = cell.color || "";
+				var leader = cell.leader || "";
+				var address = cell.address || "";
+				var meeting = cell.meeting;
+
 				var note = 
-
-				/*'<div id="content">'+
-      '<div id="siteNotice">'+
-      '</div>'+
-      '<h1 id="firstHeading" class="firstHeading">Uluru</h1>'+
-      '<div id="bodyContent">'+
-      '<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large ' +
-      'sandstone rock formation in the southern part of the '+
-      'Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) '+
-      'south west of the nearest large town, Alice Springs; 450&#160;km '+
-      '(280&#160;mi) by road. Kata Tjuta and Uluru are the two major '+
-      'features of the Uluru - Kata Tjuta National Park. Uluru is '+
-      'sacred to the Pitjantjatjara and Yankunytjatjara, the '+
-      'Aboriginal people of the area. It has many springs, waterholes, '+
-      'rock caves and ancient paintings. Uluru is listed as a World '+
-      'Heritage Site.</p>'+
-      '<p>Attribution: Uluru, <a href="http://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">'+
-      'http://en.wikipedia.org/w/index.php?title=Uluru</a> '+
-      '(last visited June 22, 2009).</p>'+
-      '</div>'+
-      '</div>';*/
-
-				'<div class="cellNote">'+
-				'<ul>' +
-					'<li><span class="cellProperty">Líder: </span><span class="cell_value">'+cell.leader+'</span></li>'+
-					'<li><span class="cellProperty">Cor: </span><span class="cell_value">'+cell.color+'</span></li>'+
-					'<li><span class="cellProperty">Endereço: </span><span class="cell_value">'+cell.address+'</span></li>'+
-					'<li><span class="cellProperty">Reuniões: </span><span class="cell_value">'+cell.meeting+'</span></li>'+
-					'</ul></div>';
-
+					'<div class="info">' +
+					'<div class="cellcolor '+color+'"></div>'+
+					'<div style="display: table-cell;">'+
+					'<ul>' +
+					'<li><b>Líder:</b> '+leader+'</li>'+
+					'<li><b>Endereço:</b> '+address+'</li>'+
+					'<li><b>Reuniões:</b> '+getMeeting(meeting)+'</li>'+
+					'</ul></div></div>';
 				return note;
 			}
 
